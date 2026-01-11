6bfb824c47e31355345b4e1542d023e0af2099bd
GiovanniOlan <giovanniolanperez@gmail.com>
2026-01-11 01:49:25 -0600

    Add swaync configuration and styling: implement notification settings, control center layout, and custom CSS theme

---
+++ hypr/scripts/lauch-apps.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Workspace 2
+sleep 2
+hyprctl dispatch workspace 2
+google-chrome-stable --profile-directory="Default" &
+sleep 1
+hyprctl dispatch workspace 11
+postman &
+sleep 1
+
+# Workspace 3
+hyprctl dispatch workspace 3
+code &
+sleep 2
+
+# Workspace 4
+hyprctl dispatch workspace 4
+brave --new-window https://mail.google.com/mail/u/0/#inbox https://mail.google.com/mail/u/1/#inbox &
+sleep 2
+
+# Workspace 5
+hyprctl dispatch workspace 5
+brave --app=https://web.whatsapp.com &
+sleep 1
+hyprctl dispatch workspace 14
+Telegram &
+sleep 2
+
+# Workspace 6
+hyprctl dispatch workspace 6
+brave --new-window https://claude.ai &
+sleep 2
+hyprctl dispatch workspace 15
+google-chrome-stable --profile-directory="Profile 1" https://chatgpt.com &
+sleep 2
+
+# Workspace 1
+hyprctl dispatch workspace 1
+kitty &
+sleep 0.5
+hyprctl dispatch workspace 10
+kitty
+sleep 0.5
+
+hyprctl dispatch split-workspace 1
